151e1ba659d2601b2d2f5ddb089a9c42e392aeb3
daltongray <dalton.roger.gray@gmail.com>
2022-12-04 16:05:05 -0500

    implement custom footer in UI

--- qwik-app/src/routes/layout.tsx
+++ qwik-app/src/routes/layout.tsx
@@ -1,10 +1,41 @@
 import { component$, Slot } from '@builder.io/qwik';
 import Header from '../components/header/header';
+import {MenuConfig, MUIFooter, ROUTES} from "~/integrations/react/layout";
+import {useLocation, useNavigate} from "@builder.io/qwik-city";
+
+export const menu = {
+    [ROUTES.EXPLORE]: {
+        href: "/upload",
+        icon: "TravelExplore",
+        index: 1,
+        text: "Explore",
+    },
+    [ROUTES.UPLOAD]: {
+        href: "/upload",
+        icon: "Drafts",
+        index: 0,
+        text: "Upload",
+    },
+}  as MenuConfig
 
 export default component$(() => {
-  return (
+
+    const { pathname } = useLocation();
+
+    const navigate = useNavigate();
+
+    return (
     <>
       <main>
+          <MUIFooter
+              menu={menu}
+              startingPage={pathname as ROUTES}
+              // @ts-ignore
+              onChange$={(page: number) => {
+                  console.log({ page });
+                  navigate.path = Object.values(menu).filter(({index})=>index === page)[0].href;
+              }}
+          />
         <Header />
         <section>
           <Slot />
